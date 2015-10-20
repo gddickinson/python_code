c546844e56eb279b4ffcca267a6c298380dcab39
George Dickinson <george.dickinson@gmail.com>
2015-10-20 12:45:15 -0700

    update

--- pythonCode-Work/distanceCalc_2.py
+++ pythonCode-Work/distanceCalc_2.py
@@ -12,13 +12,13 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 
-filename="J:\\WORK\\Nikon_System_Data_2\\test_of_photobleaching_persistance\\140318_COS_IP3R1_and_Tubulin\\140318_COS7_IP3R1_crop1_XY.txt"
+filename="C:\\Users\\George\\Desktop\\Ab_alone\\testXY_001.txt"
 storm_positions=np.fromfile(filename,sep=" ")
 storm_positions=storm_positions.reshape((len(storm_positions)/2,2))
-
+Output ="C:\\Users\\George\\Desktop\\Ab_alone\\testXY_001_result.txt"
 print("File1 loaded")
 
-filename="J:\\WORK\\Nikon_System_Data_2\\test_of_photobleaching_persistance\\140318_COS_IP3R1_and_Tubulin\\140318_COS7_Tubulin_crop1_XY.txt"
+filename="C:\\Users\\George\\Desktop\\Ab_alone\\testXY_001.txt"
 puff_site=np.fromfile(filename,sep=" ")
 puff_site=puff_site.reshape((len(puff_site)/2,2))
 
@@ -34,3 +34,6 @@ dist0  = dist0[dist0<2000]
 num_bins = 50
 
 n, bins, patches = plt.hist(dist0, num_bins, normed=0, facecolor='green', alpha=0.5)
+
+np.savetxt(Output, dist0)
+print("Result Saved")
--- pythonCode-Work/distance_to_all_neighbours_in_search_radius.py
+++ pythonCode-Work/distance_to_all_neighbours_in_search_radius.py
@@ -31,70 +31,112 @@ import math
 #FileName = "J:\\WORK\\2-colour\\IP3R1_KDEL\\XY_positions\\150224_SY5Y_IP3R1_3D_003_crop1.txt"
 #FileName2 = "J:\\WORK\\2-colour\\IP3R1_KDEL\\XY_positions\\150224_SY5Y_KDEL_3D_003_crop1.txt"
 
-FileName = "J:\\WORK\\2-colour\\IP3R1-nTerminal_UCDavisIP3R1_MonoClonal\\150224SY5Y_IP3R1_SC28614_A488_AND_UCDavis-IP3R1_L24_A647_004_UCDavis_XY_filtered.txt"
-FileName2 = "J:\\WORK\\2-colour\\IP3R1-nTerminal_UCDavisIP3R1_MonoClonal\\150224SY5Y_IP3R1_SC28614_A488_AND_UCDavis-IP3R1_L24_A647_004_UCDavis_XY_filtered.txt"
-Output = "J:\\WORK\\2-colour\\IP3R1-nTerminal_UCDavisIP3R1_MonoClonal\\150224SY5Y_IP3R1_SC28614_A488_AND_UCDavis-IP3R1_L24_A647_004_SC28614_XY_v_SC28614_result.txt"
+#FileName = "C:\\Users\\George\\Desktop\\UCDavis_labelled\\6.txt"
+#FileName2 = "C:\\Users\\George\\Desktop\\UCDavis_labelled\\6.txt"
+#Output = "C:\\Users\\George\\Desktop\\UCDavis_labelled\\6_result.txt"
 #Output = "J:\\WORK\\2-colour\\IP3R1_KDEL\\XY_positions\\150224_SY5Y_IP3R1vKDEL_001_3D_crop3_result.txt"
 
+def distances(filename1, filename2, output):
+    
+    x = np.loadtxt(filename1,skiprows=1,usecols=(0,))
+    y = np.loadtxt(filename1,skiprows=1,usecols=(1,))
+    #print('File1 Loaded')
 
-x = np.loadtxt(FileName,skiprows=0,usecols=(0,))
-y = np.loadtxt(FileName,skiprows=0,usecols=(1,))
-#print('File1 Loaded')
-x2 = np.loadtxt(FileName2,skiprows=0,usecols=(0,))
-y2 = np.loadtxt(FileName2,skiprows=0,usecols=(1,))
-print('File2 Loaded')
+    if filename2 != 'random':
+        x2 = np.loadtxt(filename2,skiprows=0,usecols=(0,))
+        y2 = np.loadtxt(filename2,skiprows=0,usecols=(1,))
+        print('File2 Loaded')
 
-###### Make array #################
-data = np.vstack((x,y))
-comparisonSet = np.vstack((x2,y2))
-##############################################
+    else:
+        x2,y2 = generateRandom(x,y)
+        print('Random points generated')
+    ###### Make array #################
+    data = np.vstack((x,y))
+    comparisonSet = np.vstack((x2,y2))
+    ##############################################
+    
+    ########### Set square search area ############
+    searchRadius = 5000
+    ##############################################
+    
+    ######## Functions ###########################
+    def getNeighbours(x, y, comparisonSet, searchRadius):
+        keptX = []
+        keptY = []
+        for i in range (comparisonSet[0].size):
+            if abs(x-comparisonSet[0][i]) < searchRadius and abs(y-comparisonSet[1][i]) < searchRadius:
+                keptX.append(comparisonSet[0][i])
+                keptY.append(comparisonSet[1][i])
+        answer = np.vstack((keptX,keptY))
+        return answer
+    
+    def getDistances(x,y, searchSet, searchRadius):
+        answer = []
+        for i in range (searchSet[0].size):
+            dist = math.sqrt((x-searchSet[0][i])*(x-searchSet[0][i])) + ((y-searchSet[1][i])*(y-searchSet[1][i]))
+            if dist < searchRadius:
+                answer.append(dist)
+        #print(answer)
+        return answer
+    
+    def allDists(dataSet,comparisonSet,searchRadius):
+        dataDist = []    
+        for i in range (dataSet[0].size):
+            distance1 = (getDistances(dataSet[0][i],dataSet[1][i],getNeighbours(dataSet[0][i],dataSet[1][i],comparisonSet,searchRadius),searchRadius))
+            for s in range (len(distance1)):
+                dataDist.append(distance1[s])
+            print((i/dataSet[0].size)*100,"%");
+        return dataDist   
+        
 
-########### Set square search area ############
-searchRadius = 1000
-##############################################
 
-######## Functions ###########################
-def getNeighbours(x, y, comparisonSet, searchRadius):
-    keptX = []
-    keptY = []
-    for i in range (comparisonSet[0].size):
-        if abs(x-comparisonSet[0][i]) < searchRadius and abs(y-comparisonSet[1][i]) < searchRadius:
-            keptX.append(comparisonSet[0][i])
-            keptY.append(comparisonSet[1][i])
-    answer = np.vstack((keptX,keptY))
-    return answer
+    #data3 = getNeighbours(data[0][3],data[1][3],comparisonSet,searchRadius)
+    #data3distance = shortestDistance(data['x'][5],data['y'][5],comparisonSet,searchRadius)
+    
+    ############# NP Array with x,y,dist #####################
+    distanceSet = allDists(data,comparisonSet,searchRadius)
+    np.savetxt(output, np.transpose(distanceSet), delimiter=',')
+    print("Result File Saved")
+    ###########################################################
+    #hist=plt.hist(distanceSet,50)
+    #print (distanceSet)
+    
+    #fig1 = plt.scatter(data[0],data[1], c='red')
+    #fig2 = plt.scatter(comparisonSet[0],comparisonSet[1], c='green')
+    #fig3 = plt.scatter(data3[0],data3[1], c='blue')
+    plt.show()
+    return
 
-def getDistances(x,y, searchSet, searchRadius):
-    answer = []
-    for i in range (searchSet[0].size):
-        dist = math.sqrt((x-searchSet[0][i])*(x-searchSet[0][i])) + ((y-searchSet[1][i])*(y-searchSet[1][i]))
-        if dist < searchRadius:
-            answer.append(dist)
-    #print(answer)
-    return answer
+def generateRandom(x,y):        
+    N = 10000
+    minX = min(x)
+    maxX = max(x)
+    minY = min(y)
+    maxY = max(y)    
 
-def allDists(dataSet,comparisonSet,searchRadius):
-    dataDist = []    
-    for i in range (dataSet[0].size):
-        distance1 = (getDistances(dataSet[0][i],dataSet[1][i],getNeighbours(dataSet[0][i],dataSet[1][i],comparisonSet,searchRadius),searchRadius))
-        for s in range (len(distance1)):
-            dataDist.append(distance1[s])
-        print((i/dataSet[0].size)*100,"%");
-    return dataDist   
+    x = np.random.random_integers(minX,maxX,N)
+    y = np.random.random_integers(minY,maxY,N) 
+
+    print(x)
+    print(y)       
+    return x,y
+
+
+path = "J:\\WORK\\Calcium_STORM\\150317\\Distance_from_puff_to_nearest_IP3R\\"
+file1 = "150317_Puff_Site_XY_P"
+file2 = file1
+
+
+
+for i in range(1,8):
+    filename1 = path + file1 + str(i) + ".txt"
+    #filename2 = path + file2 + str(i) + ".txt"
+    filename2 = 'random'
+    output = path + file2 + str(i) + "_result" + ".txt"
+    distances(filename1, filename2, output)
     
     
-#data3 = getNeighbours(data[0][3],data[1][3],comparisonSet,searchRadius)
-#data3distance = shortestDistance(data['x'][5],data['y'][5],comparisonSet,searchRadius)
-
-############# NP Array with x,y,dist #####################
-distanceSet = allDists(data,comparisonSet,searchRadius)
-np.savetxt(Output, np.transpose(distanceSet), delimiter=',')
-print("Result File Saved")
-###########################################################
-hist=plt.hist(distanceSet,50)
-#print (distanceSet)
-
-#fig1 = plt.scatter(data[0],data[1], c='red')
-#fig2 = plt.scatter(comparisonSet[0],comparisonSet[1], c='green')
-#fig3 = plt.scatter(data3[0],data3[1], c='blue')
-plt.show()
+    
+   
+    
+    
