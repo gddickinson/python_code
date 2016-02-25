cf479e0ed58429c7fa4f7479cf63aa6f006b1f72
George Dickinson <george.dickinson@gmail.com>
2016-02-25 14:09:10 -0800

    update

--- pythonCode-Work/distance_to_nearest_neighbour2.py
+++ pythonCode-Work/distance_to_nearest_neighbour2.py
@@ -32,8 +32,8 @@ Output = 'C:\\Users\\George\\Desktop\\LaserSpot2\\160128\\160128_XY_result.txt'
 #y = np.loadtxt(FileName,skiprows=1,usecols=(1,))
 #print('File1 Loaded')
 
-x2 = 55
-y2 = 44
+x2 = 79
+y2 = 19
 x = np.loadtxt(FileName2,skiprows=1,usecols=(0,))
 y = np.loadtxt(FileName2,skiprows=1,usecols=(1,))
 print('File2 Loaded')
