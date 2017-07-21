35efcb0b5204bac730b7f863aa49b17ec33ae143
George Dickinson <george.dickinson@gmail.com>
2017-07-20 18:34:57 -0600

    update

--- timelapse.py
+++ timelapse.py
@@ -8,12 +8,22 @@ Created on Thu Jul 20 17:19:16 2017
 import numpy as np
 import cv2
 import time
+import os
 
 # time delay between frames
 delay = 2.5
 
 # folder to write to
-folder = r'C:\Users\George\Desktop\timelapse\image'
+folder = r'C:\Users\George\Desktop\timelapse'
+
+subfolder = time.strftime("%d-%m-%Y_%I-%M-%S_%p")
+
+newpath = folder + '\\' + subfolder
+
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+filename = newpath + '\\' + 'img'
 
 #filetype
 fileType = '.tif'
@@ -31,7 +41,7 @@ while(1):
         
     ret, frame = cap.read()
     frame_num = "%08d" % (count,)
-    cv2.imwrite(folder + '_' + frame_num + fileType, frame)
+    cv2.imwrite(filename + '_' + frame_num + fileType, frame)
     k = cv2.waitKey(1)
     
     if k == ord('q'):
