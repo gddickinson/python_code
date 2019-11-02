1e7e27f547743dcf20123177b33ece5df3d07a1c
George Dickinson <george.dickinson@gmail.com>
2019-11-02 17:10:12 -0600

    update

---
+++ atom_IpythonKernelTest.py
@@ -0,0 +1,12 @@
+%matplotlib inline
+%config InlineBackend.figure_format = 'svg'
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.stye.use('ggplot')
+
+x = np.linespace(0,10,500)
+
+x = 2 * x
